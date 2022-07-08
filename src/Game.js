eac36ddde95990459d6bbdfe1b524212772375a0
Patrick Becker <patrickgbecker@gmail.com>
2022-07-08 12:56:25 -0500

    Add game class and index file

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,15 @@
-const data = require('./data');
+const data = require('./data.js');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = null;
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -13,6 +19,18 @@ class Game {
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+    const getCards = prototypeQuestions.map((question) => {
+      return new Card(question.id, question.question, question.answers, question.correctAnswer)
+    });
+    const deck = new Deck(getCards)
+    const round = new Round(deck)
+    this.currentRound = round
+    this.printMessage(deck)
+    this.printQuestion(round)
+  }
+
 }
 
 module.exports = Game;
